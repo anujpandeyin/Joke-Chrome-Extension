f745ca287f4770f2082c19c38e4df45c5a01e76e
anujpandeyin <officialanujpandey@gmail.com>
2023-03-01 04:10:51 +0530

    done

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// fetch('https://icanhazdadjoke.com/slack')
-//     .then(data => data.json())
-//     .then(jokeData => {
-//         const jokeText = jokeData.attachments[0].text;
-//         const jokeElement = document.getElementById('jokeElement');
-
-//         jokeElement.innerHTML = jokeText;
-//     })
-
-
-
-
-
     fetch('https://v2.jokeapi.dev/joke/Any?type=single')
     .then(data => data.json())
     .then(jokeData => {
